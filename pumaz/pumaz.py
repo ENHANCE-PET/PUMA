71a0827dedc60b8166abe720e0f3b58eadeef50c
Lalith <Lalith.shiyamsundar@meduniwien.ac.at>
2023-07-07 11:14:08 +0200

    Update pumaz.py main file

---
+++ pumaz/pumaz.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Author: Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer
+# Institution: Medical University of Vienna
+# Research Group: Quantitative Imaging and Medical Physics (QIMP) Team
+# Date: 04.07.2023
+# Version: 0.1.0
+#
+# Description:
+# The main module of the pumaz. It contains the main function that is executed when the pumaz is run.
+#
+# Usage:
+# The variables in this module can be imported and used in other modules within the pumaz.
+#
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+import argparse
+import logging
+import os
+import sys
+import time
+from datetime import datetime
+
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.INFO,
+                    filename=datetime.now().strftime('pumaz-v.1.0.0.%H-%M-%d-%m-%Y.log'),
+                    filemode='w')
+
+def main():
