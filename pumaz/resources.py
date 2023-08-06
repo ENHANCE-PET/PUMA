a47e3bcc49f3c86a5e40a46c6f16f398069143e7
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-08-06 21:49:29 +0200

    Update resources.py

--- pumaz/resources.py
+++ pumaz/resources.py
@@ -8,14 +8,25 @@
 # Date: 04.07.2023
 # Version: 0.1.0
 #
+# Module: binaries
+#
 # Description:
-# This module contains the URLs and filenames of the binaries that are required for the pumaz.
+# Amidst the labyrinth of PUMA-Z's infrastructure, the `binaries` module serves as a beacon. This module is the cornerstone 
+# of PUMA-Z's functionality, ensuring every component has the tools it needs to execute flawlessly. Like the supply chain 
+# of an intricate machine, it houses the URLs and filenames of the vital binaries, making sure that other modules don't 
+# run astray in search of them.
+#
+# Akin to a quartermaster in a grand ship, this module ensures the steady flow of essential resources. Whether a module 
+# requires the Greedy binaries for Windows or Mac, the `binaries` module holds the keys. It's not just about availability; 
+# it's about precision, making certain that every binary is exactly where it's supposed to be.
 #
 # Usage:
-# The variables in this module can be imported and used in other modules within the pumaz to download the necessary
-# binaries for the pumaz.
+# Developers and modules alike, when in need of critical binaries, turn to this module. It's a straightforward endeavor. 
+# By importing and invoking the contained variables and functions, one ensures that PUMA-Z's operations are fueled by 
+# the exact resources they demand, enabling the seamless delivery of unparalleled medical imaging services.
 #
 # ----------------------------------------------------------------------------------------------------------------------
+
 
 GREEDY_BINARIES = {
     "greedy-windows-x86_64": {
