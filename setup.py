d2cef5fa6abd25ad19ee168296469a207d43a00d
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-11-10 15:45:47 +0100

    Fix version number for pypi push.
    Signed-off-by: nutellaBear <48599863+LalithShiyam@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pumaz',
-    version='1.1.1',
+    version='1.1.2',
     author='Sebastian Gutschmayer, Lalith Kumar Shiyam Sundar',
     author_email='Sebastian.Gutschmayer@meduniwien.ac.at, Lalith.shiyamsundar@meduniwien.ac.at',
     description='PUMA (PET Universal Multi-tracer Aligner) is a robust and efficient tool for aligning images from different PET tracers. It leverages advanced diffeomorphic imaging techniques to offer high-precision alignment for multiplexed tracer images. PUMA aims to significantly enhance the accuracy and reproducibility of PET image studies.',
