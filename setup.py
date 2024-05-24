bec3c451e211de88fa1f0450d9b036ffe5306abd
mprires <manuelmoreirapires@hotmail.com>
2024-05-24 16:22:28 +0200

    Accomodated changes in MOOSE for array prediction

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pumaz',
-    version='1.6.2',
+    version='1.6.3',
     author='Lalith Kumar Shiyam Sundar, Sebastian Gutschmayer, Manuel Pires',
     author_email='Lalith.shiyamsundar@meduniwien.ac.at, Sebastian.Gutschmayer@meduniwien.ac.at, '
                  'Manuel.pires@meduniwien.ac.at',
