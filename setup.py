5b84e65a27a092b1970c077d99af916ad96ddde1
lalithShiyam <lalith.shiyamsundar@meduniwien.ac.at>
2024-11-25 19:01:58 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pumaz',
-    version='1.7.2',
+    version='1.7.3',
     author='Lalith Kumar Shiyam Sundar, Sebastian Gutschmayer, Manuel Pires',
     author_email='Lalith.shiyamsundar@meduniwien.ac.at, Sebastian.Gutschmayer@meduniwien.ac.at, '
                  'Manuel.pires@meduniwien.ac.at',
